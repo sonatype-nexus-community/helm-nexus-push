cb769e5750d71205a6917d4ccaea3054893811dc
STEVENOT Maxime <maxime.stevenot@soprabanking.com>
2019-10-20 17:11:43 +0200

    reformat

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 usage() {
-cat << EOF
+    cat <<EOF
 Push Helm Chart to Nexus repository
 
 This plugin provides ability to push a Helm Chart directory or package to a
@@ -26,44 +26,43 @@ declare NEXUS_USERNAME
 declare NEXUS_PASSWORD
 
 declare -a POSITIONAL_ARGS=()
-while [[ $# -gt 0 ]]
-do
+while [[ $# -gt 0 ]]; do
     case "$1" in
-        -h|--help)
+    -h | --help)
+        usage
+        exit 0
+        ;;
+    -u | --username)
+        if [[ -z "${2:-}" ]]; then
+            echo "Must specify username!"
+            echo "---"
             usage
-            exit 0
-            ;;
-        -u|--username)
-            if [[ -z "${2:-}" ]]; then
-                echo "Must specify username!"
-                echo "---"
-                usage
-                exit 1
-            fi
+            exit 1
+        fi
+        shift
+        NEXUS_USERNAME=$1
+        ;;
+    -p | --password)
+        if [[ -n "${2:-}" ]]; then
             shift
-            NEXUS_USERNAME=$1
-            ;;
-        -p|--password)
-            if [[ -n "${2:-}" ]]; then
-                shift
-                NEXUS_PASSWORD=$1
-            else
-                NEXUS_PASSWORD=
-            fi
-            ;;
-        *)
-            POSITIONAL_ARGS+=("$1")
-            ;;
-   esac
-   shift
+            NEXUS_PASSWORD=$1
+        else
+            NEXUS_PASSWORD=
+        fi
+        ;;
+    *)
+        POSITIONAL_ARGS+=("$1")
+        ;;
+    esac
+    shift
 done
 [[ ${#POSITIONAL_ARGS[@]} -ne 0 ]] && set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
 if [[ $# -lt 2 ]]; then
-  echo "Missing arguments!"
-  echo "---"
-  usage
-  exit 1
+    echo "Missing arguments!"
+    echo "---"
+    usage
+    exit 1
 fi
 
 indent() { sed 's/^/  /'; }
@@ -85,51 +84,51 @@ declare AUTH
 declare CHART
 
 case "$2" in
-    login)
-        if [[ -z "${NEXUS_USERNAME:-}" ]]; then
-            read -p "Username: " NEXUS_USERNAME
-        fi
-        if [[ -z "${NEXUS_PASSWORD:-}" ]]; then
-            read -s -p "Password: " NEXUS_PASSWORD
-            echo
-        fi
-        echo "$NEXUS_USERNAME:$NEXUS_PASSWORD" > "$REPO_AUTH_FILE"
-        ;;
-    logout)
-        rm -f "$REPO_AUTH_FILE"
-        ;;
-    *)
-        CMD=push
-        CHART=$2
+login)
+    if [[ -z "${NEXUS_USERNAME:-}" ]]; then
+        read -p "Username: " NEXUS_USERNAME
+    fi
+    if [[ -z "${NEXUS_PASSWORD:-}" ]]; then
+        read -s -p "Password: " NEXUS_PASSWORD
+        echo
+    fi
+    echo "$NEXUS_USERNAME:$NEXUS_PASSWORD" >"$REPO_AUTH_FILE"
+    ;;
+logout)
+    rm -f "$REPO_AUTH_FILE"
+    ;;
+*)
+    CMD=push
+    CHART=$2
 
-        if [[ -z "${NEXUS_USERNAME:-}" ]] || [[ -z "${NEXUS_PASSWORD:-}" ]]; then
-            if [[ -f "$REPO_AUTH_FILE" ]]; then
-                echo "Using cached login creds..."
-                AUTH="$(cat $REPO_AUTH_FILE)"
-            else
-                if [[ -z "${NEXUS_USERNAME:-}" ]]; then
-                    read -p "Username: " NEXUS_USERNAME
-                fi
-                if [[ -z "${NEXUS_PASSWORD:-}" ]]; then
-                    read -s -p "Password: " NEXUS_PASSWORD
-                    echo
-                fi
-                AUTH="$NEXUS_USERNAME:$NEXUS_PASSWORD"
-            fi
+    if [[ -z "${NEXUS_USERNAME:-}" ]] || [[ -z "${NEXUS_PASSWORD:-}" ]]; then
+        if [[ -f "$REPO_AUTH_FILE" ]]; then
+            echo "Using cached login creds..."
+            AUTH="$(cat $REPO_AUTH_FILE)"
         else
+            if [[ -z "${NEXUS_USERNAME:-}" ]]; then
+                read -p "Username: " NEXUS_USERNAME
+            fi
+            if [[ -z "${NEXUS_PASSWORD:-}" ]]; then
+                read -s -p "Password: " NEXUS_PASSWORD
+                echo
+            fi
             AUTH="$NEXUS_USERNAME:$NEXUS_PASSWORD"
         fi
+    else
+        AUTH="$NEXUS_USERNAME:$NEXUS_PASSWORD"
+    fi
 
-        if [[ -d "$CHART" ]]; then
-            CHART_PACKAGE="$(helm package "$CHART" | cut -d":" -f2 | tr -d '[:space:]')"
-        else
-            CHART_PACKAGE="$CHART"
-        fi
+    if [[ -d "$CHART" ]]; then
+        CHART_PACKAGE="$(helm package "$CHART" | cut -d":" -f2 | tr -d '[:space:]')"
+    else
+        CHART_PACKAGE="$CHART"
+    fi
 
-        echo "Pushing $CHART to repo $REPO_URL..."
-        curl -is -u "$AUTH" "$REPO_URL" --upload-file "$CHART_PACKAGE" | indent
-        echo "Done"
-        ;;
+    echo "Pushing $CHART to repo $REPO_URL..."
+    curl -is -u "$AUTH" "$REPO_URL" --upload-file "$CHART_PACKAGE" | indent
+    echo "Done"
+    ;;
 esac
 
 exit 0
