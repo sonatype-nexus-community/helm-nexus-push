2f8299b09657961d67b1d5c193d0540b28a0b038
Gustavo Oliveira <gustavo.oliveira.1@icloud.com>
2022-05-26 17:36:05 -0300

    Update push.ps1

--- push.ps1
+++ push.ps1
@@ -148,7 +148,7 @@ switch ($globalArgs[1]) {
         $REPO_URL += "/${CHART_PACKAGE}"
         $username = $AUTH.split(":")[0]
         $password = $AUTH.split(":")[1]
-        $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AUTH.split(":")[0],($AUTH.split(":")[1] | ConvertTo-SecureString)
+        $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username,$password
         Write-Host "Pushing $CHART to repo $REPO_URL..."
         $bytes = [System.Text.Encoding]::ASCII.GetBytes($AUTH)
         $base64 = [System.Convert]::ToBase64String($bytes)
